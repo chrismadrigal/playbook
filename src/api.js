81852448ae65262f3b2162d2dba0e168bc7ed97b
Chrismadrigal <crismas07@gmail.com>
2018-11-23 15:49:12 -0800

    Update API comments

--- src/api.js
+++ src/api.js
@@ -1,22 +1,19 @@
 import axios from 'axios';
 
 /**
- * This endpoint is to get the user profile information
+ * This endpoint is used for fetching the user profile information.
  *
- * @param  {JSON Object} data               This is a JSON object containing contextID
- * @param  {Axios token} axiosCancelToken   This is a cancel token sent from the api call
- * @return {Response Object}                The response can be hooked from a .then or .catch method on the api call
+ * @return {Response Object}  The response can be hooked from a .then or .catch method on the api call
  */
 export const getProfileInfo = () => axios.get(
     'https://s3.amazonaws.com/dribbble/frontend-exercise/user.json',
 );
 
 /**
- * This endpoint is to get the user profile information
+ * This endpoint is used for fetching the playbook shots besed on a page.
  *
- * @param  {JSON Object} data               This is a JSON object containing contextID
- * @param  {Axios token} axiosCancelToken   This is a cancel token sent from the api call
- * @return {Response Object}                The response can be hooked from a .then or .catch method on the api call
+ * @param  {Number} page      The page number to append to the endpoint url.
+ * @return {Response Object}  The response can be hooked from a .then or .catch method on the api call
  */
 export const getShots = (page) => axios.get(
     `https://s3.amazonaws.com/dribbble/frontend-exercise/shots-${page}.json`,
